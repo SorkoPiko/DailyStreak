a115f39eed7b207046ef774ce50d4f8c797e0360
Orion Railean <orion.railean@gmail.com>
2025-05-29 16:46:25 +1000

    fix #4

--- src/hooks/DailyLevelPage.cpp
+++ src/hooks/DailyLevelPage.cpp
@@ -15,6 +15,7 @@ bool DSDailyLevelPage::init(const GJTimedLevelType type) {
 
     m_fields->m_label = CCLabelBMFont::create(streakString.c_str(), colour);
     m_fields->m_label->setScale(0.4f);
+    m_fields->m_label->setZOrder(-1);
     m_fields->m_label->setAnchorPoint({0, 0});
     m_fields->m_label->setPosition({(winSize.width / 2) - 183 + xOffset, (winSize.height / 2) + 51});
     m_fields->m_label->setID("streak-label"_spr);
